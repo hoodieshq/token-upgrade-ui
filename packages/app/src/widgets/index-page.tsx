f3d420bd14e88f5c471147c4ffa9998ff7254e4c
Sergo <rogaldh@radsh.red>
2024-03-27 15:08:45 +0100

    Pass the cluster's moniker to the link

--- packages/app/src/widgets/index-page.tsx
+++ packages/app/src/widgets/index-page.tsx
@@ -1,12 +1,24 @@
-import { TokenUpgrade, useNotificationContext } from "@solana/token-upgrade-ui"
-import { Pattern } from "../shared/pattern"
-import { TOKEN_UPGRADE_PROGRAM_ID } from "../env"
-import { useCallback, useEffect, useState } from "react"
+import * as web3 from "@solana/web3.js"
 import ChangeCluster from "../features/change-cluster"
 import Input from "../shared/input"
-import * as web3 from "@solana/web3.js"
+import { Pattern } from "../shared/pattern"
+import { TOKEN_UPGRADE_PROGRAM_ID } from "../env"
+import { TokenUpgrade, useNotificationContext } from "@solana/token-upgrade-ui"
+import { useCallback, useEffect, useState } from "react"
+import { useConnection } from "@solana/wallet-adapter-react"
+
+function getCluster(rpc: string) {
+  function getMoniker(s: string): web3.Cluster | "custom" {
+    if (s === web3.clusterApiUrl("devnet")) return "devnet"
+    if (s === web3.clusterApiUrl("mainnet-beta")) return "mainnet-beta"
+    if (s === web3.clusterApiUrl("testnet")) return "testnet"
+    return "custom"
+  }
+  return getMoniker(rpc)
+}
 
 export default function IndexPage() {
+  const { connection } = useConnection()
   const { setNotification } = useNotificationContext()
 
   const _log = useCallback(
@@ -43,7 +55,7 @@ export default function IndexPage() {
             onUpgradeEnd={({ signature }) =>
               setNotification({
                 message: "Token upgraded",
-                link: `https://explorer.solana.com/tx/${signature}`,
+                link: `https://explorer.solana.com/tx/${signature}?cluster=${getCluster(connection.rpcEndpoint)}`,
               })
             }
             onUpgradeError={(error) =>
